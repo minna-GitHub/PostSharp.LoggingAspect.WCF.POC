eb092e550da5bac0ae3644b62f91dc21bb91b261
Minna Rao <35445853+minna-GitHub@users.noreply.github.com>
2018-11-07 16:49:19 +0530

    Add files via upload
    version 1.0.0.0

---
+++ PostSharp.LoggingAspect.WCF.POC/PostSharp.LoggingAspect.WCF.POC/SampleService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PostSharp.LoggingAspect.WCF.POC
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class SampleService : ISampleService
+    {
+        public string GetData(int value)
+        {
+            CallSamePrivateMethod("hi", "wcf");
+            return string.Format("You entered: {0}", value);
+        }
+
+        private void CallSamePrivateMethod(string a, string b)
+        {
+            string c = string.Concat(a, b);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
---
+++ PostSharp.LoggingAspect.WCF.POC/PostSharp.LoggingAspect.WCF.POC/WebServiceAspect.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using PostSharp.Aspects;
+using System.Diagnostics;
+
+namespace PostSharp.LoggingAspect.WCF.POC
+{
+    [Serializable]
+    [WebServiceAspect(AttributeExclude = true)]
+    public class WebServiceAspect: OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            args.MethodExecutionTag = Stopwatch.StartNew();
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Stopwatch sw = (Stopwatch)args.MethodExecutionTag;
+            sw.Stop();
+            string methodName = (args.Method != null ? args.Method.Name : "");
+            string className = string.Empty, arguments = string.Empty;
+            bool logFile = true, isPrivateMethod = false;
+            if (string.IsNullOrEmpty(methodName).Equals(false))
+            {
+                if (args.Method.DeclaringType != null)
+                {
+                    if (args.Method.DeclaringType.Name.Equals("CompositeType"))
+                    {
+                        logFile = false;
+                    }
+                    else
+                    {
+                        className = $"{args.Method.DeclaringType.Namespace}.{args.Method.DeclaringType.Name}";
+                        arguments = string.Join(", ", args.Arguments);
+                        isPrivateMethod = (args.Method.IsPrivate ? true : false);
+                    }
+                }
+
+
+
+            }
+
+            if (logFile)
+            {
+                string output = string.Format("{0} method {1}.{2}() with arguments {3} is Executed in {4} seconds",
+                    (isPrivateMethod ? "Private" : "Public"), className, methodName, arguments,
+                    sw.ElapsedMilliseconds / 1000);
+
+                AppendToFile(output);
+                System.Diagnostics.Debug.WriteLine(output);
+            }
+        }
+        private void AppendToFile(string line)
+        {
+            System.IO.File.AppendAllLines(@"\\192.168.0.50\Share\R&D\bin\log.txt", new string[] {DateTime.Now.ToString()+ Environment.NewLine+ line });
+            Console.WriteLine(">> " + line);
+        }
+    }
+}
